b5a6530d464807d15987bd3681a85f5702d5d694
uysalhalil <uysal.halil@outlook.com>
2026-03-04 12:11:16 -0500

    node with random pointer

---
+++ problems/Node.h
@@ -0,0 +1,14 @@
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = nullptr;
+        random = nullptr;
+    }
+};
